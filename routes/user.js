ac4f23e3863b4dfa8ea95466898b9876ce55cd2e
Joel Lara Quintana <joel.lq@hotmail.com>
2017-04-21 19:38:38 -0500

    Added index

---
+++ routes/user.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+    res.render('user', {
+        title: 'Usuario',
+        user: {
+            id: req.session.user_id,
+            name: req.session.user_name,
+            username:req.session.user_username
+        }
+    });
+});
+
+module.exports = router;
